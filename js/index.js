8bcf3d2f0a93d87e14a24dcde456f0a6ec4fe1a1
TranThanhQuan <quanlive123@gmail.com>
2022-12-23 14:31:52 +0700

    update submit form email

--- js/index.js
+++ js/index.js
@@ -76,6 +76,100 @@ function showPopupMb() {
 }
 
 
+// submit form
+var form = document.getElementById("promise_form");
+function handleForm (event) {event.preventDefault();}
+form.addEventListener('submit', handleForm);
+
+
+function insert(){
+    var email = $("input[name=email]").val();
+
+    if( email == '' ){show_result({ title: "Thông báo !", msg: 'Vui lòng nhập email !' });return false;}
+
+
+    $.ajax({
+        url: 'backend/function.php',
+        type: "POST",
+        data:{
+            email: email,
+            action: "insert"
+        },
+        success: function(res){
+            // console.log(res);
+            // console.log(res.msg);
+            // console.log(res.status);
+
+            if(res == "failed"){
+                show_result({"title":"Thông báo !","msg":`Email đã tồn tại`,"redirect":"/"});
+
+            }
+            else {
+                show_result({"title":"Thông báo !","msg":`Đăng ký email thành công !`,"redirect":"/"});
+            }
+        }
+
+    })
+
+}
+
+
+
+//function Hien thi thong bao
+function show_result(response, callback){
+    var title_choose, title_arr;
+    title_arr = ['Lỗi !', 'Chúc mừng !','Thông báo', 'Thông báo !'];
+
+    title_choose = ( typeof response.title == "undefined" ? title_arr[response.status] : response.title );
+    let className='',text='Ok',btn_ok_visible=true;
+
+    if(response.status==1){
+        // className='vq_notice';
+        // title_choose='';
+    }else{
+         text='Ok';
+         btn_ok_visible=true; 
+    }
+
+    setTimeout(function(){
+        var div = document.createElement("div");
+        div.innerHTML = response.msg + "<a href='javascript:;' onclick='close_swal()' class=' close_popup'></a>";
+        swal({
+            title: title_choose,
+            content:div,
+            className: className,
+            buttons:{
+                confirm:{
+                    text: text,//Đăng nhập
+                    value: "confirm",
+                    visible: btn_ok_visible,
+                    className: "btn_ok",
+                    closeModal: true
+                }
+            }
+        }).then((willDelete) => {
+            if( typeof response.reload != 'undefined' ){
+                //Lam moi lại trang
+                console.log("ok reload");
+                location.reload();
+            }
+            
+            if( typeof response.redirect !== 'undefined' ){
+                //Chuyen huong trang
+                console.log("ok redirect");
+                window.location.href = response.redirect;
+            }
+            if (typeof callback !== 'undefined') {
+                //Callback function other
+                console.log("ok callback");
+
+                callback();
+                return;
+            }
+        });
+    },200);
+
+}
 
 
 
